f4b99fb74485eef2cf800cd38565ab4019e78f1a
plzheheplztrying <tryinhplzheehe83@gmail.com>
2025-07-31 05:35:26 -0400

    Update POpenShell.php

--- PHP/POpenShell.php
+++ PHP/POpenShell.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Command Executor</title>
+
+    <!---- # TeamRedHead --- >
 </head>
 <body>
     <h2>Execute Command</h2>
